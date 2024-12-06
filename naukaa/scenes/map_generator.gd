bf077fd7607eefab24b9ebd5d16920261ebf24fb
tomekblox <tomekniemiec9@gmail.com>
2024-12-06 19:55:45 +0100

    ae2

--- naukaa/scenes/map_generator.gd
+++ naukaa/scenes/map_generator.gd
@@ -1,32 +1,31 @@
 extends Node2D
 
-@export var image_path : String = "res://heightmap.png" # Ścieżka do obrazu
-@export var tilemap_node : Node
+@export var image_path : String = "res://sources/jeden.png" # Ścieżka do obrazu
+@export var tilemap_node : TileMapLayer
 @export var layer_id : int = 0 # Warstwa TileMap
 @export var source_ids : Dictionary = { 0: 1, 85: 2, 170: 3, 255: 4 } # Mapowanie kolorów na source_id
+var width = 4
+var height = 4
 
 func _ready():
 	var image = Image.new()
 	image.load(image_path) # Wczytaj obraz
-	var tilemap = $tilemap_node as TileMapLayer
+	var tilemap = tilemap_node as TileMapLayer
 	tilemap = tilemap_node
 	#image.lock()
 	
 	for x in range(image.get_width()):
 		for y in range(image.get_height()):
 			var color = image.get_pixel(x, y)
-			var grayscale = int(color.r * 255) # Odcień szarości
-			var source_id = find_source_id(grayscale)
+			var grayscale = int(color.r * 4) # Odcień szarości
+			var source_id = find_source_id(grayscale,grayscale)
 			
 			if source_id != null:
 				var coords = Vector2i(x, y)
-				tilemap.set_cell(coords,source_id, ne)
+				tilemap.set_cell(coords,layer_id, source_id)
 				
 	
-	image.unlock()
 
-func find_source_id(value: int) -> int:
-	for threshold in source_ids.keys():
-		if value <= threshold:
-			return source_ids[threshold]
-	return -1 
+func find_source_id(value: int,value2: int) -> Vector2i:
+	#return Vector2i(round(value),round(value2))
+	return Vector2i(3,round(value2))
